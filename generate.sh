2d4f192a2e31d75c3389ec6d575474667079bfb8
Pavol Pidanič <paulp@hotmail.sk>
2017-06-02 10:33:08 +0200

    script for creating antlr generated files (removed from repository)

---
+++ generate.sh
@@ -0,0 +1,3 @@
+java -jar antlr4-4.5.3.jar Saral.g4 -visitor -package com.pidanic.saral.grammar -encoding UTF8
+
+mv Saral.tokens SaralBaseListener.java SaralBaseVisitor.java SaralLexer.java SaralLexer.tokens SaralListener.java SaralParser.java SaralVisitor.java src/main/java/com/pidanic/saral/grammar
