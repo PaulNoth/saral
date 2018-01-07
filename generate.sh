9351c92edd50efc5b9e3f1268bdf100b95a8c640
Paul P <paulp@hotmail.sk>
2018-01-07 16:33:35 +0100

    updated to antlr 4.7.1

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-java -jar antlr4-4.5.3.jar Saral.g4 -visitor -package com.pidanic.saral.grammar -encoding UTF8
+java -jar antlr-4.7.1.jar Saral.g4 -visitor -package com.pidanic.saral.grammar -encoding UTF8
 
-mv Saral.tokens SaralBaseListener.java SaralBaseVisitor.java SaralLexer.java SaralLexer.tokens SaralListener.java SaralParser.java SaralVisitor.java src/main/java/com/pidanic/saral/grammar
+mv Saral.tokens SaralBaseListener.java SaralBaseVisitor.java SaralLexer.java SaralLexer.tokens SaralListener.java SaralParser.java SaralVisitor.java Saral.interp SaralLexer.interp src/main/java/com/pidanic/saral/grammar
