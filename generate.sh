678250451e240e30d8e85bc55eacad569374c19b
Paul P <paulp@hotmail.sk>
2018-04-17 20:37:56 +0200

    antlr jars

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-java -jar antlr-4.5.3-complete.jar Saral.g4 -visitor -package com.pidanic.saral.grammar -encoding UTF8
+java -jar bin/antlr-4.5.3-complete.jar Saral.g4 -visitor -package com.pidanic.saral.grammar -encoding UTF8
 
 mv Saral.tokens SaralBaseListener.java SaralBaseVisitor.java SaralLexer.java SaralLexer.tokens SaralListener.java SaralParser.java SaralVisitor.java src/main/java/com/pidanic/saral/grammar
