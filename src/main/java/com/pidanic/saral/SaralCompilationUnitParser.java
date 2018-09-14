08d879ddcb07a500ae568912b2618907e694bc74
Pavol Pidanič <paulp@hotmail.sk>
2018-09-14 09:32:48 +0200

    replaced deprecated class

--- src/main/java/com/pidanic/saral/SaralCompilationUnitParser.java
+++ src/main/java/com/pidanic/saral/SaralCompilationUnitParser.java
@@ -5,10 +5,7 @@ import com.pidanic.saral.error.SaralTreeWalkErrorListener;
 import com.pidanic.saral.grammar.SaralLexer;
 import com.pidanic.saral.grammar.SaralParser;
 import com.pidanic.saral.visitor.InitVisitor;
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,7 +16,7 @@ public class SaralCompilationUnitParser {
         String fileAbsPath = file.getAbsolutePath();
         String className = fileName.substring(0, fileName.lastIndexOf('.'));
 
-        CharStream charStream = new ANTLRFileStream(fileAbsPath);
+        CharStream charStream = CharStreams.fromFileName(fileAbsPath);
         SaralLexer saralLexer = new SaralLexer(charStream);
         CommonTokenStream commonTokenStream  = new CommonTokenStream(saralLexer);
         SaralParser saralParser = new SaralParser(commonTokenStream);
